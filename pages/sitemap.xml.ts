b962351d0a3532c11b06c664dc686d7e82337b1c
2nonnon <lanpangzi0205@gmail.com>
2023-04-22 23:59:20 +0800

    robots、sitemap 更新

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,28 +1,46 @@
 import type { GetServerSideProps } from 'next'
 import { getAllPostIds } from '../lib/posts'
 
-const DOMAIN = 'https://blog.nnln.love'
+const DOMAIN = 'https://www.nnln.love'
 
 function generateSiteMap(posts: ReturnType<typeof getAllPostIds>, locales: string[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <url>
-       <loc>${DOMAIN}</loc>
-     </url>
-     <url>
-       <loc>${`${DOMAIN}/posts`}</loc>
-     </url>
+     ${locales
+    .map((locale) => {
+      return ` 
+        <url>
+          <loc>${DOMAIN}${locale}</loc>
+        </url>
+        <url>
+          <loc>${`${DOMAIN}${locale}/posts`}</loc>
+        </url>
+        <url>
+          <loc>${`${DOMAIN}${locale}/toys`}</loc>
+        </url>
+        <url>
+          <loc>${`${DOMAIN}${locale}/toys/color`}</loc>
+        </url>
+        <url>
+          <loc>${`${DOMAIN}${locale}/toys/image`}</loc>
+        </url>
+        <url>
+          <loc>${`${DOMAIN}${locale}/toys/minesweeper`}</loc>
+        </url>
+        <url>
+          <loc>${`${DOMAIN}${locale}/toys/qrcode`}</loc>
+        </url>
+        `
+    }).join('')}
      ${posts
     .map(({ params }) => {
-      return locales.reduce((res, locale) => {
-        return `${res}
+      return locales.map((locale) => {
+        return `
           <url>
-            <loc>${`${DOMAIN}/posts/${locale}/${params.id}`}</loc>
-          </url>`
-      }, `
-      <url>
-          <loc>${`${DOMAIN}/posts/${params.id}`}</loc>
-      </url>`)
+            <loc>${`${DOMAIN}/posts${locale}/${params.id}`}</loc>
+          </url>
+          `
+      }).join('')
     })
     .join('')}
    </urlset>
@@ -38,7 +56,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res, locales, def
   const posts = getAllPostIds()
 
   // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts, locales.filter(locale => locale !== defaultLocale))
+  const sitemap = generateSiteMap(posts, locales.map(locale => locale === defaultLocale ? '' : `/${locale}`))
 
   res.setHeader('Content-Type', 'text/xml')
   // we send the XML to the browser
