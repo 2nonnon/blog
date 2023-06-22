deeb0c683f73e02bcde3cf6f03c2365dd24babbb
2nonnon <lanpangzi0205@gmail.com>
2023-06-22 20:07:22 +0800

    feat: / rewrite to /en/，保持原路由

--- middleware.ts
+++ middleware.ts
@@ -42,7 +42,7 @@ export function middleware(request: NextRequest) {
 
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
-    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
+    return NextResponse.rewrite(new URL(`/${locale}${pathname}`, request.url))
   }
 }
 
