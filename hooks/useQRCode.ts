103976650f6f134d71527f8c5bd6276d0381d686
2nonnon <lanpangzi0205@gmail.com>
2023-06-29 00:08:12 +0800

    feat: add useQrcode

---
+++ hooks/useQRCode.ts
@@ -0,0 +1,42 @@
+import qrcode from 'qrcode-generator'
+import { useState } from 'react'
+
+export const ErrorCorrectionLevelMap = {
+  L: 'L(7%)',
+  M: 'M(15%)',
+  Q: 'Q(25%)',
+  H: 'H(30%)',
+}
+
+export const ModeMap = {
+  Byte: 'Byte',
+  Numeric: 'Numeric',
+  Alphanumeric: 'Alphanumeric',
+  Kanji: 'Kanji',
+}
+
+export const MultibyteMap = {
+  'default': 'none',
+  'UTF-8': 'UTF-8',
+}
+
+type MultibyteType = keyof typeof MultibyteMap
+
+interface QRCodeOptions {
+  typeNumber: TypeNumber
+  errorCorrectionLevel: ErrorCorrectionLevel
+  mode: Mode
+  content: string
+  multibyte: MultibyteType
+}
+
+export const useQRCode = (initOptions: QRCodeOptions) => {
+  const [options, setOptions] = useState(initOptions)
+
+  qrcode.stringToBytes = qrcode.stringToBytesFuncs[options.multibyte]
+  const qr = qrcode(options.typeNumber, options.errorCorrectionLevel)
+  qr.addData(options.content)
+  qr.make()
+
+  return [qr, setOptions]
+}
