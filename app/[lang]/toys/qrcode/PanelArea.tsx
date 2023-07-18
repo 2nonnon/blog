9e4ae78e11779d2379414254905c219ae902b38d
2nonnon <lanpangzi0205@gmail.com>
2023-07-18 22:58:55 +0800

    refactor: 简化事件处理函数

--- app/[lang]/toys/qrcode/PanelArea.tsx
+++ app/[lang]/toys/qrcode/PanelArea.tsx
@@ -5,7 +5,6 @@ import { useCallback } from 'react'
 import { Icon } from '@iconify-icon/react'
 import { useQrcodeDispatch, useQrcodeOptions } from './QrcodeContext'
 import { ErrorCorrectionLevelMap, MarkerStyleMap, PixelStyleMap } from '@/components/Qrcode'
-import type { MarkerStyleType, PixelStyleType } from '@/components/Qrcode'
 
 const FormItem: FC<{ name: string; children: any }> = ({ name, children }) => {
   return (
@@ -18,18 +17,18 @@ const FormItem: FC<{ name: string; children: any }> = ({ name, children }) => {
   )
 }
 
-const Upload: FC<{ value: File; onChange: ChangeEventHandler<HTMLInputElement>; onClear: MouseEventHandler<HTMLButtonElement> }> = ({ value, onChange, onClear }) => {
+const Upload: FC<{ name: string; value: File; onChange: ChangeEventHandler<HTMLInputElement>; onClear: MouseEventHandler<HTMLButtonElement> }> = ({ name, value, onChange, onClear }) => {
   return (<>
     <div className='flex items-center gap-2 min-w-0'>
       <label className='surface-sm flex items-center justify-center w-fit rounded px-4 py-1 gap-2 cursor-pointer'>
-        <input id='Logo' className='hidden' type="file" accept='image/*' onChange={onChange}/>
+        <input data-key={name} className='hidden' type="file" accept='image/*' onChange={onChange}/>
         <Icon icon="mingcute:upload-2-line" />
         <span>Upload</span>
       </label>
       {
         value && <div className='flex items-center gap-2 min-w-0'>
           <p className='overflow-hidden text-ellipsis' title={value.name}>{value.name}</p>
-          <button type='button' className='flex text-xl' onClick={onClear}><Icon icon="jam:close-circle-f" /></button>
+          <button data-key={name} type='button' className='flex text-xl' onClick={onClear}><Icon icon="jam:close-circle-f" /></button>
         </div>
       }
     </div>
@@ -41,62 +40,34 @@ export default function Panel() {
 
   const QrcodeDispatch = useQrcodeDispatch()
 
-  const clearBackground = useCallback((_event: React.MouseEvent<HTMLButtonElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { background: undefined } })
+  const clearUpload = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
+    const key = event.currentTarget.dataset.key as string
+    key && QrcodeDispatch({ type: 'changed', options: { [key]: undefined } })
   }, [])
 
-  const clearLogo = useCallback((_event: React.MouseEvent<HTMLButtonElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { logo: undefined } })
+  const handleUpload = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const key = event.currentTarget.dataset.key as string
+    key && QrcodeDispatch({ type: 'changed', options: { [key]: event.target.files![0] } })
   }, [])
 
-  const handleChangeBackground = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { background: event.target.files![0] } })
-  }, [])
-
-  const handleChangeLogo = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { logo: event.target.files![0] } })
-  }, [])
-
-  const handleChangePixelStyle = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { pixelStyle: event.target.value as PixelStyleType } })
-  }, [])
-
-  const handleChangeMarkerStyle = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { markerStyle: event.target.value as MarkerStyleType } })
-  }, [])
-
-  const handleChangeErrorCorrectionLevel = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { errorCorrectionLevel: event.target.value as ErrorCorrectionLevel } })
-  }, [])
-
-  const handleChangeContent = useCallback((event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { content: event.target.value } })
-  }, [])
-
-  const handleChangeTypeNumber = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { typeNumber: +event.target.value as TypeNumber } })
-  }, [])
-
-  const handleChangePixelSize = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { pixelSize: +event.target.value } })
-  }, [])
-
-  const handleChangeMargin = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    QrcodeDispatch({ type: 'changed', options: { margin: +event.target.value } })
+  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const key = event.currentTarget.dataset.key as string
+    const value = typeof options[key] === 'number' ? +event.target.value : event.target.value
+    key && QrcodeDispatch({ type: 'changed', options: { [key]: value } })
   }, [])
 
   return (
     <form className='flex flex-col gap-3 text-base pb-4'>
       <fieldset className='flex flex-col gap-3 w-full min-w-[auto]'>
         <div className='border border-[var(--border-color)] flex rounded'>
-          <textarea id="Content" className='w-full rounded px-4 py-2' rows={5} value={options.content} onChange={handleChangeContent}></textarea>
+          <textarea data-key='content' id="Content" className='w-full rounded px-4 py-2' rows={5} value={options.content} onChange={handleChange}></textarea>
         </div>
 
         <FormItem name='ErrorCorrectionLevel'>
           <div className='surface-sm__inert w-fit rounded flex overflow-hidden'>
             {Object.entries(ErrorCorrectionLevelMap).map(item =>
               <div className='border-r last:border-none border-[var(--border-color)]' key={item[0]}>
-                <input id={item[1]} className='peer hidden' type="radio" name='ErrorCorrectionLevel' value={item[0]} onChange={handleChangeErrorCorrectionLevel} checked={options.errorCorrectionLevel === item[0]}></input>
+                <input data-key='errorCorrectionLevel' id={item[1]} className='peer hidden' type="radio" name='ErrorCorrectionLevel' value={item[0]} onChange={handleChange} checked={options.errorCorrectionLevel === item[0]}></input>
                 <label htmlFor={item[1]} className='peer-checked:surface-sm__active px-2 py-1 flex cursor-pointer'>{item[1]}</label>
               </div>)}
           </div>
@@ -104,7 +75,7 @@ export default function Panel() {
 
         <FormItem name='TypeNumber'>
           <div className='border border-[var(--border-color)] flex rounded w-fit'>
-            <select className='rounded w-32' id='TypeNumber' name="TypeNumber" value={options.typeNumber} onChange={handleChangeTypeNumber}>
+            <select data-key='typeNumber' className='rounded w-32' id='TypeNumber' name="TypeNumber" value={options.typeNumber} onChange={handleChange}>
               <option value={0}>Auto Detect</option>
               {Array.from({ length: 40 }).map((_, i) => <option value={i + 1} key={i + 1}>{i + 1}</option>)}
             </select>
@@ -115,15 +86,15 @@ export default function Panel() {
       <fieldset className='flex flex-col gap-3 w-full min-w-0'>
         <FormItem name='PixelSize'>
           <div className='flex gap-4 w-full'>
-            <input className='flex-1' type="range" min={1} max={100} value={options.pixelSize} onChange={handleChangePixelSize}/>
-            <input className='border border-[var(--border-color)] rounded w-16 pl-1' type="number" min={1} max={100} value={options.pixelSize} onChange={handleChangePixelSize}/>
+            <input data-key='pixelSize' className='flex-1' type="range" min={1} max={100} value={options.pixelSize} onChange={handleChange}/>
+            <input data-key='pixelSize' className='border border-[var(--border-color)] rounded w-16 pl-1' type="number" min={1} max={100} value={options.pixelSize} onChange={handleChange}/>
           </div>
         </FormItem>
 
         <FormItem name='Margin'>
           <div className='flex gap-4 w-full'>
-            <input className='flex-1' type="range" min={0} max={25} value={options.margin} onChange={handleChangeMargin}/>
-            <input className='border border-[var(--border-color)] rounded w-16 pl-1' type="number" min={1} max={100} value={options.margin} onChange={handleChangeMargin}/>
+            <input data-key='margin' className='flex-1' type="range" min={0} max={25} value={options.margin} onChange={handleChange}/>
+            <input data-key='margin' className='border border-[var(--border-color)] rounded w-16 pl-1' type="number" min={1} max={100} value={options.margin} onChange={handleChange}/>
           </div>
         </FormItem>
 
@@ -131,7 +102,7 @@ export default function Panel() {
           <div className='surface-sm__inert w-fit rounded flex overflow-hidden'>
             {Object.entries(PixelStyleMap).map(item =>
               <div className='border-r last:border-none border-[var(--border-color)]' key={item[0]}>
-                <input id={item[0]} className='peer hidden' type="radio" name='PixelStyle' value={item[0]} onChange={handleChangePixelStyle} checked={options.pixelStyle === item[0]}></input>
+                <input data-key='pixelStyle' id={item[0]} className='peer hidden' type="radio" name='PixelStyle' value={item[0]} onChange={handleChange} checked={options.pixelStyle === item[0]}></input>
                 <label htmlFor={item[0]} className='peer-checked:surface-sm__active px-2 py-1 flex cursor-pointer'>
                   {item[1]}
                 </label>
@@ -143,7 +114,7 @@ export default function Panel() {
           <div className='surface-sm__inert w-fit rounded flex overflow-hidden'>
             {Object.entries(MarkerStyleMap).map(item =>
               <div className='border-r last:border-none border-[var(--border-color)]' key={`${item[0]}M`}>
-                <input id={`${item[0]}M`} className='peer hidden' type="radio" name='MarkerStyle' value={item[0]} onChange={handleChangeMarkerStyle} checked={options.markerStyle === item[0]}></input>
+                <input data-key='markerStyle' id={`${item[0]}M`} className='peer hidden' type="radio" name='MarkerStyle' value={item[0]} onChange={handleChange} checked={options.markerStyle === item[0]}></input>
                 <label htmlFor={`${item[0]}M`} className='peer-checked:surface-sm__active px-2 py-1 flex cursor-pointer' >
                   {item[1]}
                 </label>
@@ -152,11 +123,11 @@ export default function Panel() {
         </FormItem>
 
         <FormItem name='Logo'>
-          <Upload value={options.logo} onChange={handleChangeLogo} onClear={clearLogo}></Upload>
+          <Upload name='logo' value={options.logo} onChange={handleUpload} onClear={clearUpload}></Upload>
         </FormItem>
 
         <FormItem name='Background'>
-          <Upload value={options.background} onChange={handleChangeBackground} onClear={clearBackground}></Upload>
+          <Upload name='background' value={options.background} onChange={handleUpload} onClear={clearUpload}></Upload>
         </FormItem>
       </fieldset>
     </form>
