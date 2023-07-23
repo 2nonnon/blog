9c04d8d36a5fb3b663baebd183e857a4dbf1d581
2nonnon <lanpangzi0205@gmail.com>
2023-07-23 22:23:39 +0800

    feat: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,39 @@
 import type { MetadataRoute } from 'next'
 import { getSortedPostsData } from '@/lib/posts'
 import { i18n } from '@/i18n-config'
+import { getAllToysPath } from '@/lib/toys'
 
 const DOMAIN = 'https://www.nnln.love'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const posts = getSortedPostsData()
+  const toys = getAllToysPath()
+  const locales = ['', ...i18n.locales.map(locale => `/${locale}`)]
 
-  return i18n.locales.map((locale) => {
+  return locales.map((locale) => {
     return [
       {
-        url: `${DOMAIN}/${locale}`,
+        url: `${DOMAIN}${locale}`,
         lastModified: new Date(),
       },
       {
-        url: `${DOMAIN}/${locale}/posts`,
-        lastModified: new Date(),
-      },
-      {
-        url: `${DOMAIN}/${locale}/toys`,
+        url: `${DOMAIN}${locale}/posts`,
         lastModified: new Date(),
       },
       ...posts.map(({ title, update }) => (
         {
-          url: `${`${DOMAIN}/${locale}/posts/${title}`}`,
+          url: `${`${DOMAIN}${locale}/posts/${title}`}`,
           lastModified: new Date(update),
+        }
+      )),
+      {
+        url: `${DOMAIN}${locale}/toys`,
+        lastModified: new Date(),
+      },
+      ...toys.map(name => (
+        {
+          url: `${`${DOMAIN}${locale}/toys/${name}`}`,
+          lastModified: new Date(),
         }
       )),
     ]
