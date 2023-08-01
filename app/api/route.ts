2263d4137b50e87af4c144020477138c797f1e5e
2nonnon <lanpangzi0205@gmail.com>
2023-08-01 22:01:39 +0800

    feat: ip

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const requestHeaders = new Headers(request.headers)
+  const ip = requestHeaders.get('cf-connecting-ip')
+  const ipAddress = requestHeaders.get('cf-ipcountry')
 
-  return NextResponse.json({ headers: Array.from(requestHeaders.entries()) })
+  return NextResponse.json({ IP: ip, IPAddress: ipAddress })
 }
