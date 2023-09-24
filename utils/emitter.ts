7fc5637e98246c456ba35f42317cc2b6405e59fc
2nonnon <lanpangzi0205@gmail.com>
2023-09-24 17:53:56 +0800

    feat: emitter

--- utils/emitter.ts
+++ utils/emitter.ts
@@ -1,34 +1,37 @@
-export type TaskEvent = string
-interface Fn<T = any, R = T> {
-  (...arg: T[]): R
-}
+interface DefaultOpts { [x: string]: (...args: any[]) => void }
 
-export class PubSub {
-  private eventMap = new Map<TaskEvent, Fn[]>()
+type Parameters<T extends (...args: any) => any> = T extends (...args: infer P) => any
+  ? P extends any[]
+    ? P
+    : never
+  : never
 
-  subscribe(event: TaskEvent, callback: Fn) {
+export class Emitter<T extends DefaultOpts = DefaultOpts, K extends keyof T = keyof T> {
+  private eventMap = new Map<K, Array<T[K]>>()
+
+  subscribe<E extends K>(event: E, callback: T[E]) {
     if (this.eventMap.has(event))
       this.eventMap.get(event)?.push(callback)
     else this.eventMap.set(event, [callback])
   }
 
-  unsubscribe(event: TaskEvent, callback: Fn) {
+  unsubscribe<E extends K>(event: E, callback: T[E]) {
     const cbQueue = this.eventMap.get(event)
     const index = cbQueue?.findIndex(cb => cb === callback)
     if (typeof index === 'number')
       cbQueue?.splice(index, 1)
   }
 
-  once(event: TaskEvent, callback: Fn) {
+  once<E extends K>(event: E, callback: T[E]) {
     const fn = (...args: any[]) => {
       callback(...args)
-      this.unsubscribe(event, fn)
+      this.unsubscribe(event, fn as any)
     }
 
-    this.eventMap.get(event)?.push(fn)
+    this.eventMap.get(event)?.push(fn as any)
   }
 
-  emit(event: TaskEvent, ...args: any[]) {
+  emit<E extends K>(event: E, ...args: Parameters<T[E]>) {
     this.eventMap.get(event)?.forEach(callback => callback(...args))
   }
 }
